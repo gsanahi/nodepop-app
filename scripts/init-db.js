2594dbe9f2e633a6a471b8bf74e56d82c1cc30d2
Anahi Garcia <gsanahi@gmail.com>
2023-01-07 19:59:19 +0100

    fix warnings

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -1,12 +1,16 @@
 // getting-started.js
 const mongoose = require('mongoose');
+mongoose.set('strictQuery',true);
 const Ad = require('../models/ad');
 const data = require('./data-db');
 //array de objetos
 
 async function main() {
     await mongoose.connect('mongodb://192.168.0.17:27017/nodepop-app');
+    // limpiar base de datos antes de inicializarla
     await Ad.collection.drop().catch(() => {});
+    // me aseguro de crear todos los indices
+    await Ad.ensureIndexes();
 
     for(let i=0;i< data.length;i++) {
         const ad = data[i];
